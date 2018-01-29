055ef77bbefa1d9c86cb2ee4821d2683d10e19ac
Vinicius Pacheco <vin175pacheco@gmail.com>
2018-01-29 02:32:01 -0200

    Changed how importing works + Add option to resolvePath fn + Changed build to dist

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -6,17 +6,17 @@ import plugin from '../lib/plugin'
 const fixturesPath = path.resolve(__dirname, './fixtures')
 
 function readFile(folder, fileName) {
-  const filePath = path.join(fixturesPath, folder, `${ fileName }.css`)
+  const filePath = path.join(fixturesPath, folder, `${fileName}.css`)
   return {
     path: filePath,
-    css: fs.readFileSync(filePath).toString()
+    css: fs.readFileSync(filePath).toString(),
   }
 }
 
 function readInAndOut(fileName) {
   return {
     source: readFile('in', fileName),
-    expected: readFile('out', fileName)
+    expected: readFile('out', fileName),
   }
 }
 
@@ -24,25 +24,21 @@ describe('Basics', () => {
   it('should handle basic hashing and scoping', async () => {
     const { source, expected } = readInAndOut('basics')
 
-    const result = await postcss([plugin()])
-      .process(source.css, { from: source.path })
+    const result = await postcss([plugin()]).process(source.css, { from: source.path })
 
     expect(result.css).toEqual(expected.css)
     expect(result.translations).toEqual({
       className: '_className_1wre7_h4',
-      subClass: '_subClass_kp1bo_b'
+      subClass: '_subClass_kp1bo_b',
     })
   })
 
   it('should have different translations based on sourcePath', async () => {
     const { source } = readInAndOut('basics')
 
-    const result = await postcss([plugin()])
-      .process(source.css, { from: source.path })
+    const result = await postcss([plugin()]).process(source.css, { from: source.path })
 
-
-    const result2 = await postcss([plugin()])
-      .process(source.css, { from: `${ source.path }.` })
+    const result2 = await postcss([plugin()]).process(source.css, { from: `${source.path}.` })
 
     expect(result.translations).not.toEqual(result2.translations)
   })
@@ -58,7 +54,8 @@ describe('Basics', () => {
 
     expect(result.css).toEqual(expected.css)
     expect(result.translations).toEqual({
-      className: '_className_1iqoc_tu'
+      className: '_className_1iqoc_tu',
+      subClass: '_subClass_1ix19_4h',
     })
   })
 })
